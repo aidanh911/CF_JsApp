fa45c074ddd90bc1766e579aab2ccf417f4179ad
hogi <aidanh911@gmail.com>
2023-04-14 15:09:52 -0500

    task 3 code updated for formatting

--- js/task3.js
+++ js/task3.js
@@ -1,12 +1,12 @@
 let pokemonList = [
-    {name: 'Bulbasaur', type: 'Grass', height: '7'},
-    {name: 'Charmander', type: 'Fire', height: '6'},
-    {name: 'Caterpie', type: 'Bug', height: '3'}
+    {name: 'Bulbasaur', type: 'Grass', height: '.7'},
+    {name: 'Charmander', type: 'Fire', height: '.6'},
+    {name: 'Caterpie', type: 'Bug', height: '.3'}
 ];
 
 for (let i= 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height <= 5) {
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's small!<br>");
+    if (pokemonList[i].height >= .7) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!<br>");
     } else {
         document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
     }
