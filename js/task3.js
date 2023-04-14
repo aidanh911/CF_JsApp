463a1de8d2a49da443fa4c8ebc672f915dc7d379
hogi <aidanh911@gmail.com>
2023-04-13 23:29:02 -0500

    task 3 code

---
+++ js/task3.js
@@ -0,0 +1,9 @@
+let pokemonList = [
+    {name: 'Bulbasaur', type: 'Grass', height: '7'},
+    {name: 'Charmander', type: 'Fire', height: '6'},
+    {name: 'Caterpie', type: 'Bug', height: '3'}
+];
+
+for (let i= 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + pokemonList[i].height);
+};
