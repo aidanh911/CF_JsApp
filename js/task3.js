303576a126b59ab553d39e583fce636f9ff6b445
hogi <aidanh911@gmail.com>
2023-04-14 00:19:52 -0500

    task 3 code

--- js/task3.js
+++ js/task3.js
@@ -5,5 +5,9 @@ let pokemonList = [
 ];
 
 for (let i= 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + pokemonList[i].height);
+    if (pokemonList[i].height <= 5) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's small!<br>");
+    } else {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
+    }
 };
