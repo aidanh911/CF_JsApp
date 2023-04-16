020f13cf42ed40f5a47c934e8fec3e161c6547d1
hogi <aidanh911@gmail.com>
2023-04-16 01:49:22 -0500

    this is the updated js code with IIFE to make the pokemonList array not globally accesible and updated functions to loop through the array

--- js/task3.js
+++ js/task3.js
@@ -1,17 +1,22 @@
-let pokemonList = [
-    {name: 'Bulbasaur', type: 'Grass', height: '.7'},
-    {name: 'Charmander', type: 'Fire', height: '.6'},
-    {name: 'Caterpie', type: 'Bug', height: '.3'}
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Bulbasaur', type: 'Grass', height: '.7'},
+        {name: 'Charmander', type: 'Fire', height: '.6'},
+        {name: 'Caterpie', type: 'Bug', height: '.3'}
+    ];
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    };
+    function getAll() {
+        return pokemonList;
+    };
 
-// for (let i= 0; i < pokemonList.length; i++) {
-//     if (pokemonList[i].height >= .7) {
-//         document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!<br><br>");
-//     } else {
-//         document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br><br>");
-//     }
-// };
+    return {
+        add: add,
+        getAll: getAll
+    };
+}) ()
 
-pokemonList.forEach(function(pokemon) {
-    console.log(pokemon.name + 'is' + pokemon.height + 'meters tall and is a ' + pokemon.type + ' type');
-})
+pokemonRepository.getAll().forEach(function (pokemon) {
+    console.log(pokemon.name + ' is ' + pokemon.height + ' meters tall and is a ' + pokemon.type + ' type');
+});
