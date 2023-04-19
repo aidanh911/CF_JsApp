f7200f9e1b1274301b401a98c4ea8c3fc907fdae
hogi <aidanh911@gmail.com>
2023-04-19 02:23:17 -0500

    added buttons for pokemon onto the pokemon list ul with click event listener

--- js/task3.js
+++ js/task3.js
@@ -1,30 +1,50 @@
 let pokemonRepository = (function () {
-    let Repository = [
-        {name: 'Bulbasaur', type: 'Grass', height: '.7'},
-        {name: 'Charmander', type: 'Fire', height: '.6'},
-        {name: 'Caterpie', type: 'Bug', height: '.3'}
+    let repository = [
+        {name: "Bulbasaur", type: "Grass", height: ".7"},
+        {name: "Charmander", type: "Fire", height: ".6"},
+        {name: "Caterpie", type: "Bug", height: ".3"}
     ];
     function add(pokemon) {
         if (
             typeof pokemon === "object" &&
             "name" in pokemon &&
             "height" in pokemon &&
-            "types" in pokemon)
-        {Repository.push(pokemon)}
+            "type" in pokemon)
+        {repository.push(pokemon)}
         else {
             console.log("pokemon is not correct")
-        };
-    };
+        }
+    }
     function getAll() {
-        return Repository;
-    };
+        return repository;
+    }
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listPokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listPokemon.appendChild(button);
+        pokemonList.appendChild(listPokemon);
+
+        button.addEventListener("click", function(event){
+            showDetails(pokemon)
+        })
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon)
+    }
 
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem
     };
 }) ()
+pokemonRepository.add({name: "Pikachu", height: 0.3, type: ["electric"] })
 
+console.log(pokemonRepository.getAll());
 pokemonRepository.getAll().forEach(function (pokemon) {
-    document.write(pokemon.name + ' is ' + pokemon.height + ' meters tall and is a ' + pokemon.type + ' type,<br>' );
+    pokemonRepository.addListItem(pokemon);
 });
