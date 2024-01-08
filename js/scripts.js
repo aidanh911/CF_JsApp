5e4135235edcac13beaa7914a7cf7e453ca7704d
hogi <aidanh911@gmail.com>
2024-01-08 17:45:07 -0600

    finalized styled app

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function () {
         let pokemonList = [];
-        let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=20';
+        let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=200';
 
 
         function add(pokemon) {
